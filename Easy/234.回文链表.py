75f5bc5c86d71e88c0544175f8ce06bb304c695c
611de <309255056@qq.com>
2025-07-12 16:59:47 +0800

    Add solution for "回文链表" problem

---
+++ Easy/234.回文链表.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=234 lang=python3
+#
+# [234] 回文链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        vals = []
+        p = head
+        while p:
+            vals.append(p.val)
+            p = p.next
+        return vals == vals[::-1]
+# @lc code=end
+
