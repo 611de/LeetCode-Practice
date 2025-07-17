961cd4532c9a6260f7827e0797a649b3c12d6f32
611de <309255056@qq.com>
2025-07-17 11:34:28 +0800

    Add solution for "搜索二维矩阵" problem

---
+++ Medium/74.搜索二维矩阵.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=74 lang=python3
+#
+# [74] 搜索二维矩阵
+#
+
+# @lc code=start
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        return any(True if target in line else False for line in matrix)
+# @lc code=end
+
