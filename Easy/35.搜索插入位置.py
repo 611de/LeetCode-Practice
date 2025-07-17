e7869b15a9416a382eac4fd5ce27bc8a6eb49386
611de <309255056@qq.com>
2025-07-17 11:26:46 +0800

    Add solution for "搜索插入位置" problem

---
+++ Easy/35.搜索插入位置.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=35 lang=python3
+#
+# [35] 搜索插入位置
+#
+
+# @lc code=start
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+            elif nums[i]<target:
+                continue
+            else:
+                return i
+        return len(nums) 
+# @lc code=end
+
