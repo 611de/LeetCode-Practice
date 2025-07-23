03a0469c821526d3c9af9d1dafbbc3ff10acfc91
611de <309255056@qq.com>
2025-07-23 21:45:29 +0800

    Add solution for "每日温度" problem

--- Medium/739.每日温度.py
+++ Medium/739.每日温度.py
@@ -7,6 +7,14 @@
 # @lc code=start
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        
+        stack=[]
+        res = [0]*len(temperatures)
+        for i in range(len(temperatures)):
+            while stack and temperatures[i]>temperatures[stack[-1]]:
+                index = stack.pop(-1)
+                res[index] = i-index
+            stack.append(i)
+        return res
+             
 # @lc code=end
 
