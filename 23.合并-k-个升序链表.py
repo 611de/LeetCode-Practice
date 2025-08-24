0727828cac8891ada03fb256e3667d4f263a3cad
611de <309255056@qq.com>
2025-08-24 23:07:58 +0800

    Add solution for "合并 K 个升序链表" problem

---
+++ 23.合并-k-个升序链表.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode.cn id=23 lang=python3
+#
+# [23] 合并 K 个升序链表
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if len(lists)==0:
+            return None
+        if len(lists)==1:
+            return lists[0]
+
+        def mergeTwo(list1, list2):
+            dummy = ListNode()
+            cur = dummy
+            while list1 and list2:
+                if list1.val < list2.val:
+                    cur.next = list1
+                    list1 = list1.next
+                else:
+                    cur.next = list2
+                    list2 = list2.next
+                cur = cur.next
+            cur.next = list1 if list1 else list2
+            return dummy.next
+        
+        head1 = self.mergeKLists(lists=lists[:len(lists)//2])
+        head2 = self.mergeKLists(lists=lists[len(lists)//2:])
+        return mergeTwo(head1, head2)
+
+            
+# @lc code=end
+
