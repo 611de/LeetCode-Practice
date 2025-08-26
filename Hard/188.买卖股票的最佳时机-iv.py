e7586ab94100bd84a12ceea9377653f503f12625
611de <309255056@qq.com>
2025-08-26 23:11:19 +0800

    Add solution for "买卖股票的最佳时机 IV" problem

---
+++ Hard/188.买卖股票的最佳时机-iv.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode.cn id=188 lang=python3
+#
+# [188] 买卖股票的最佳时机 IV
+#
+
+# @lc code=start
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        n = len(prices)
+        @cache
+        def dfs(i, j, hold):
+            if i<0:
+                return -inf if hold else 0
+            if j<0:
+                return -inf
+            if hold:
+                return max(dfs(i-1, j, hold), dfs(i-1, j, not hold) - prices[i])
+            return max(dfs(i-1, j, hold), dfs(i-1, j-1, not hold) + prices[i])
+        return dfs(n-1, k, False)
+         
+# @lc code=end
+
